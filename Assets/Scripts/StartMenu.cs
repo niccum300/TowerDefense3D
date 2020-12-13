52e359fc6b4843eef6a032b9826e3b6455062111
niccum299 <niccum299@gmail.com>
2020-12-13 16:42:59 -0500

    Added Start Screen

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public string StartScene;
+
+    public void SwapScene()
+    {
+        SceneManager.LoadScene(StartScene);
+    }
+}
