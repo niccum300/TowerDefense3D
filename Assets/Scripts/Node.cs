3d1e0a277c7b1d61067062b9d35287efec4e557e
niccum299 <niccum299@gmail.com>
2020-12-13 03:52:43 -0500

    Added Turret Placement and Valid Placement Markers

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Material hoverMaterial;
+    public Material hoverMaterialError;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Material startMaterial;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startMaterial = rend.material;
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            rend.material = hoverMaterialError;
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        if (turret != null)
+        {
+            rend.material = hoverMaterialError;
+            return;
+        }
+
+        rend.material = hoverMaterial;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material = startMaterial;
+    }
+}
