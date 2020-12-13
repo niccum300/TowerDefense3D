1056344b453bc0b55d8bdf59077fb32b001d14f7
niccum299 <niccum299@gmail.com>
2020-12-13 02:43:25 -0500

    initial Commit

---
+++ Assets/Images/Map/blocks.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="blocks" tilewidth="16" tileheight="16" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="cobblestone.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="dirt.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="grass_block_top.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="grass_path_top.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="sandstone_top.png"/>
+ </tile>
+</tileset>
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 0f;
+
+    public Text waveCountdownText;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Math.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        waveIndex++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
